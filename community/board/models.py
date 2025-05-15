b24d95057d2a864b36a1db5123e0c7dc4fa498a3
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-05-15 17:06:20 +0900

    feat. Add Post, Comment Model

--- community/board/models.py
+++ community/board/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField()
+    body = models.TextField()
+    created_at = models.DateField()
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateField()
+    comment = models.CharField()
