ea36680a51edce10884b8e5f387bc17b1cc8fc27
jiyonggg <kimvergognoso21@hufs.ac.kr>
2025-05-15 17:51:03 +0900

    chore. Add auto_now_add to created_at

--- community/board/models.py
+++ community/board/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField()
     body = models.TextField()
-    created_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    created_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
     comment = models.CharField()
